273069644617cfd630beed5a8333f12351d22563
Ivan Martinez <ivan.martinez.h@alumnos.udg.mx>
2016-12-06 18:19:26 -0600

    Agregada la clase de imagen y sonido

---
+++ project_8_pt1.py
@@ -0,0 +1,14 @@
+class Imagen():
+    def tamarindo(self):
+        pass
+    def escala_grises(self):
+        pass
+
+    def binarizacion(self):
+        pass
+
+class sonido():
+    def reproducir(self):
+        pass
+
+    
